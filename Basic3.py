771d25a6e3891791d62ff8ea5047d20e763e7aed
MUMTAHINA-766 <64628178+MUMTAHINA-766@users.noreply.github.com>
2022-01-24 09:03:48 +0600

    Add files via upload
    While loop and for loop

---
+++ Basic3.py
@@ -0,0 +1,18 @@
+#while loop
+
+x = 0
+while x <=5:
+    print(x)
+    x = x+1
+print('count finished')
+
+#for loop
+
+for i in range(0,5):
+    print(i)
+print('finished')
+for i in range(12,20,2):
+    print(i)
+    print('Mumtahina')
+for i in range(5,-4,-1):
+    print(i)
