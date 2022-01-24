d3306b8c62f2dd4e8fba8ced68b5f33cf16a1109
MUMTAHINA-766 <64628178+MUMTAHINA-766@users.noreply.github.com>
2022-01-24 08:34:36 +0600

    Add files via upload

---
+++ quize1.py
@@ -0,0 +1,11 @@
+##if true print output
+
+# a = 2 ** 3 + 'python'*2 + 5//2
+# print(a)
+
+# ai_master = 'Andrew Ng' * 2 * 2
+# print(ai_master)
+
+#1 == 2 and 2 ** 2 == 2 ** 3 or ('python' != 'java' and (15//27//9 == 5) )
+
+#_o = 10
