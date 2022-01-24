9c69c29e22fc5234d8cef03376015772dbd62bd1
MUMTAHINA-766 <64628178+MUMTAHINA-766@users.noreply.github.com>
2022-01-24 08:46:17 +0600

    Add files via upload

---
+++ Basic2.py
@@ -0,0 +1,13 @@
+#conditional statement
+
+print('input your command')
+robot = input()
+
+if robot == 'front':
+    print('moving front')
+elif robot == 'back':
+    print('moving back')
+else:
+    print('stand still')
+
+
