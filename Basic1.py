b7c7948e5263c0df449172b9d289a0daf2589a78
MUMTAHINA-766 <64628178+MUMTAHINA-766@users.noreply.github.com>
2022-01-24 07:48:49 +0600

    Add files via upload

---
+++ Basic1.py
@@ -0,0 +1,31 @@
+#practice 1: swapping
+
+from cmath import sqrt
+from re import A
+import math
+
+
+a = 10
+b = 20
+
+a, b = b, a
+
+print(a,b)
+
+#practice 2: Quadratic equation with real solution
+
+#x^2-5x+6 = 0
+
+
+a = (-(-5)+(math.sqrt(pow(-5, 2) - (4*6))))/2
+
+b= (-(-5)-(math.sqrt(pow(-5, 2) - (4*6))))/2
+
+print('x = ', a , 'and' , b)
+
+#practice 3: temperature converter C to F
+
+Celcius = 100
+F = ((9*Celcius)/5) + 32
+print(F)
+
