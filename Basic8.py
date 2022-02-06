ed292c6125b4821699e95d98a62cbaa6231ed6b5
MUMTAHINA-766 <64628178+MUMTAHINA-766@users.noreply.github.com>
2022-02-06 09:01:54 +0600

    Update Basic8.py

--- Basic8.py
+++ Basic8.py
@@ -40,8 +40,3 @@ else:
 """
 
 
-
-
-#highest common factor between two numbers(factor-if a is divided by b then b is a factor)
-#write a small function to depict a rock-paper-scissors game.your function should take two inputs and should give us the winner based on a certain condition.
-
