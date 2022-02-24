35e0d07c0a0617e8b0ade76d09d7b20cdcff29e4
MUMTAHINA-766 <64628178+MUMTAHINA-766@users.noreply.github.com>
2022-02-24 11:34:00 +0600

    Add files via upload

---
+++ string_formate.py
@@ -0,0 +1,9 @@
+print('I am {} and i am {} years old'.format('Mumtahina','23'))
+print('I am {1} and i am {0}'.format('Mumtahina','sad'))
+
+#another way
+name = 'Mumtahina'
+food = 'Noodles'
+IDE = 'vscode'
+
+print(f'I am {name} and my favourite food is {food}\nI use {IDE}\nAnd {2**2}=={4}')
